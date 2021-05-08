07851fea4ac340e9dabe2662b153d735d32359a6
retse <anast.rets@hotmail.com>
2021-05-08 14:17:06 +0300

    Solve problem 3

---
+++ problem_3.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+
+int max_prime_factor(long long int number)
+{
+    long long int counter, max_prime, product=1;
+
+    // Remove 2s from search
+    while(number%2==0)
+    {
+        number = number/2;   
+    }
+
+    // Main searching loop
+    for(counter=3; counter <= sqrt(number); counter += 2)
+    {
+        if (number%counter == 0){
+            product = 1*counter;
+            while(number%counter==0)
+            {
+                max_prime = counter;
+                number = number/counter;
+            }
+        }
+    }
+    max_prime = number;
+    return max_prime;
+}
+
+int main()
+{
+    long long int result;
+    long long int n = 600851475143;
+    result = max_prime_factor(n);
+    printf("The result is %lld", result);
+
+    return 0;
+}
