9209392d5e91c2525f6616d13f31034d1b633b4a
retse <anast.rets@hotmail.com>
2021-05-08 17:46:41 +0300

    Return check outcome as boolean

--- solutions/problem_004.c
+++ solutions/problem_004.c
@@ -7,20 +7,39 @@ int palindrome_number_check(int n)
     // Checks if a number is palindrome
     // Input is number n 
     // Output is 0 if n is not palindrome or 1 if n is palindrome
-    int counter;
-    for(counter=0; counter< MAX_DIGITS; counter++)
+    
+    int counter, outcome=1;
+    int digits_array[MAX_DIGITS];
+
+    // Log digits in array
+    for(counter=0; counter < MAX_DIGITS; counter++)
     {
-        printf("%d", n%10);
+        digits_array[counter] = n % 10;
         n = n/10;
     }
-    return 0;
+
+    // Check if palindrome
+    for(counter=0; counter < MAX_DIGITS/2; counter++)
+    {
+        if(digits_array[counter]==digits_array[MAX_DIGITS-1-counter])
+        {
+            continue;
+        }
+        else
+        {
+            outcome = 0;
+            break;
+        }
+        
+    }
+    return outcome;
 }
 
 int main()
 {
-    int number = 998001;
-    int result;
-    result = palindrome_number_check(number);
-    printf("The result is %d", result);
+    int first_number, second_number;
+    int number = 102231;
+    first_number = palindrome_number_check(number);
+    printf("Result is %d", first_number);
     return 0;
 }
