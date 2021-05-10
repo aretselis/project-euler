55c1ee0ac7c9732f6f413b06e672666f7534e7d3
retse <anast.rets@hotmail.com>
2021-05-10 13:49:51 +0300

    Include speedy solution

--- solutions/problem_005.c
+++ solutions/problem_005.c
@@ -37,12 +37,12 @@ int *prime_factoring(int number)
     return factor_array;
 }
 
-int main()
+void naive_solution(int number)
 {
     int i, j, k, max, counter, results_array[MAX_NUMBER][SIZE];
     long int result;
     int *pointer;
-    pointer = prime_factoring(20);
+    pointer = prime_factoring(number);
 
     // Initialize results array
     for (i = 0; i < MAX_NUMBER; i++)
@@ -90,6 +90,58 @@ int main()
         }
         max = 0;
     }
+    printf("The result is %li\n", result);
+}
+
+void speedy_solution(int number)
+{
+    long int result=1; 
+    int i, j, counter, temp, prime_check=1;
+    counter = 0;
+    // Deal with 2s first
+    while (result<number)
+    {
+        result *= 2;
+        if(result>number)
+        {
+            result /= 2;
+            break;
+        }
+    }
+
+    // Check for the rest of the primes
+    for(i=3; i<=number; i+=2)
+    {
+        for(j=2; j<i; j++)
+        {
+            if (i%j==0)
+            {
+                prime_check = 0;
+            }
+        }
+        if(prime_check==1)
+        {
+            temp = i;
+            while (temp<number)
+            {
+                temp *= i;
+                if(temp>number)
+                {
+                    temp /= i;
+                    break;
+                }
+            }
+            result *= temp;
+        }
+        prime_check = 1;
+    } 
     printf("The result is %li", result);
+}
+
+int main()
+{
+    int n = 20;
+    naive_solution(n);
+    speedy_solution(n);
     return 0;
 }
